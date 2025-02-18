3c835f08d349154e87a59300adcc0233d72c903c
leohkg <leohkg@163.com>
2025-02-18 17:03:59 +0800

    add icon 'mdi'

--- src/assets/plugins/vuetify.ts
+++ src/assets/plugins/vuetify.ts
@@ -1,9 +1,13 @@
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { md3 } from 'vuetify/blueprints'
 
 export default createVuetify({
+	icons: {
+		defaultSet: 'mdi',
+	},
 	blueprint: md3,
   	components,
   	directives,
